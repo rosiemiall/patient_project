7c7d7bea13da73f360dd06e68032b1dd0e1702c6
Rosie Miall <rosiemiall@Rosies-Air.ultrahub>
2024-03-28 12:35:28 +0000

    added functionality for get request of all rooms and room by id in service and controller

--- patient_project_server/src/main/java/com/example/patient_project/controllers/RoomController.java
+++ patient_project_server/src/main/java/com/example/patient_project/controllers/RoomController.java
@@ -1,13 +1,34 @@
 package com.example.patient_project.controllers;
 
+import com.example.patient_project.models.Room;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.patient_project.services.RoomService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value= "rooms")
 public class RoomController {
     @Autowired
     RoomService roomService;
+
+    @GetMapping
+    public ResponseEntity<List<Room>> getAllRooms(){
+        return new ResponseEntity<>(roomService.getAllRooms(), HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Room> getRoomById(@PathVariable Long id) {
+        Optional<Room> roomByID = roomService.getRoomById(id);
+        return new ResponseEntity<>(
+                roomByID.orElse(null),
+                roomByID.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK
+        );
+    }
 }
--- patient_project_server/src/main/java/com/example/patient_project/services/RoomService.java
+++ patient_project_server/src/main/java/com/example/patient_project/services/RoomService.java
@@ -1,10 +1,14 @@
 package com.example.patient_project.services;
 
+import com.example.patient_project.models.Room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.patient_project.repositories.BedRepository;
 import com.example.patient_project.repositories.PatientRepository;
 import com.example.patient_project.repositories.RoomRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService {
@@ -18,4 +22,11 @@ public class RoomService {
     RoomRepository roomRepository;
 
     //check occupied beds? and then set this in room model
+    public List<Room> getAllRooms(){
+        return roomRepository.findAll();
+    }
+
+    public Optional<Room> getRoomById(Long id){
+        return roomRepository.findById(id);
+    }
 }
