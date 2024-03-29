ffdc8c3d3c8ad14d36a7ef6d69adbdf4f5007fd4
Rosie Miall <rosiemiall@Rosies-Air.ultrahub>
2024-03-29 12:49:14 +0000

    added function to add patient in patient service

--- patient_project_server/src/main/java/com/example/patient_project/controllers/PatientController.java
+++ patient_project_server/src/main/java/com/example/patient_project/controllers/PatientController.java
@@ -1,4 +1,9 @@
 package com.example.patient_project.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "patients")
 public class PatientController {
 }
