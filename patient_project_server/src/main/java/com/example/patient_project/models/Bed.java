b7608095394b26774b875e077bec1fba2cc802e4
Rosie Miall <rosiemiall@Rosies-Air.ultrahub>
2024-03-29 12:08:43 +0000

    json ignore properties in bed class

--- patient_project_server/src/main/java/com/example/patient_project/models/Bed.java
+++ patient_project_server/src/main/java/com/example/patient_project/models/Bed.java
@@ -1,6 +1,7 @@
 package com.example.patient_project.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 @Entity
@@ -16,7 +17,7 @@ public class Bed {
     private Room room;
 
     @OneToOne //(mappedBy = "bed_id")
-    @JsonIgnore
+    @JsonIgnoreProperties({"bed"})
     private Patient patient;
 
     @Column
